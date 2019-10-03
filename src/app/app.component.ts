f924652612e409e9fb27246bd6e6b19b15e63653
arnabbhat82 <arnabbhat@gmail.com>
2019-10-03 14:16:04 +0530

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'reactive-forms';
+  signupForm: FormGroup;
 }
